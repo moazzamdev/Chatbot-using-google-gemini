a3fbd5007e91a3461cc86597af9ad82e7ec2f632
Moazzam Riaz <126562859+moazzamdev@users.noreply.github.com>
2023-12-31 19:21:10 +0500

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from page1 import text
+from page2 import image
+def main():
+
+    st.title("Chat With Gemini")
+
+    with st.sidebar:
+        selection = option_menu(
+        menu_title="Main Menu",
+        options=["Text Model", "Image Model"],
+        icons=["pencil", "image"],
+        menu_icon="cast",
+        default_index=0
+        )
+
+    if selection == "Text Model":
+        text()
+
+    elif selection == "Image Model":
+        image()
+
+
+if __name__ == '__main__':
+    main()
